a20865a123b0be679ec9a5bdb76a754a09efb0f3
Waleed Raza <53474473+wrbmf@users.noreply.github.com>
2025-11-03 20:35:05 -0500

    Update site.js

--- assets/site.js
+++ assets/site.js
@@ -1,6 +1,4 @@
-<script>
 (function(){
-  // Highlight current nav item based on file name
   const links = document.querySelectorAll('.navlinks a');
   const here = location.pathname.split('/').pop() || 'index.html';
   links.forEach(a=>{
@@ -9,8 +7,6 @@
       a.classList.add('active');
     }
   });
-  // Footer year
   const y = document.getElementById('year'); if(y){ y.textContent = new Date().getFullYear(); }
 })();
-</script>
 
